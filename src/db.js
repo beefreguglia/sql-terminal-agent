b8d859b63baaa263c31dddf4af021bed8d60c523
Bernardo <beefreguglia@gmail.com>
2026-09-08 16:12:16 -0300

    feature: create database

---
+++ src/db.js
@@ -0,0 +1,25 @@
+import { DatabaseSync } from 'node:sqlite'
+
+export function createDb(path = ":memory:") {
+  const db = new DatabaseSync(path)
+
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS access_logs (
+      ip TEXT NOT NULL,
+      username TEXT NOT NULL,
+      first_name TEXT NOT NULL,
+      last_name TEXT NOT NULL,
+      email TEXT NOT NULL,
+      location TEXT NOT NULL,
+      job_area TEXT NOT NULL,
+      company TEXT NOT NULL,
+      job_title TEXT NOT NULL,
+      id TEXT PRIMARY KEY,
+      timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    )
+  `)
+
+  return db
+}
+
+const db = createDb("access_logs.db")
